dfde21bf570defe7bccb2d5e4495cd43bb72a9cc
Eduardo Herzer <eduardo.herzer@gmail.com>
2020-11-23 19:51:20 -0300

    service/bestSeller: Implement getBestSellers function

--- Best Sellers/Best Sellers/service/BestSellerService.swift
+++ Best Sellers/Best Sellers/service/BestSellerService.swift
@@ -6,10 +6,42 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
 
 class BestSellerService: BestSellerServiceProtocol {
+    private let apiKey = ""
+    
     func getBestSellers(success: @escaping ([BestSeller]) -> Void, failure: @escaping (String) -> Void) {
-        //TODO
-        failure("ERRO")
+        let request = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-nonfiction.json"
+        let parameters = ["api-key": apiKey]
+        
+        if apiKey.isEmpty {
+            failure("Please inform an API Key at BestSellerService class")
+            return
+        }
+        
+        AF.request(request, method: .get, parameters: parameters).responseJSON { (response) in
+            if let responseError = response.error {
+                failure(responseError.localizedDescription)
+                return
+            }
+            
+            do {
+                if let responseData = response.data {
+                    let jsonResponse = try JSON(data: responseData)
+                    let jsonArray = jsonResponse["results"]["books"]
+                    
+                    if let bestSellers = try? JSONDecoder().decode([BestSeller].self, from: jsonArray.rawData()) {
+                        success(bestSellers)
+                        return
+                    }
+                }
+                
+                failure("Could not load best seller list")
+            } catch {
+                failure(error.localizedDescription)
+            }
+        }
     }
 }
