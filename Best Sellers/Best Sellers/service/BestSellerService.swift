5dffba2f1fee52e9f590d0e009cbb3bec3457f5e
Eduardo Herzer <eduardo.herzer@gmail.com>
2020-11-23 07:21:05 -0300

    service: Create best seller service

---
+++ Best Sellers/Best Sellers/service/BestSellerService.swift
@@ -0,0 +1,15 @@
+//
+//  BestSellerService.swift
+//  Best Sellers
+//
+//  Created by Eduardo Herzer on 23/11/20.
+//
+
+import Foundation
+
+class BestSellerService: BestSellerServiceProtocol {
+    func getBestSellers(success: @escaping ([BestSeller]) -> Void, failure: @escaping (String) -> Void) {
+        //TODO
+        failure("ERRO")
+    }
+}
