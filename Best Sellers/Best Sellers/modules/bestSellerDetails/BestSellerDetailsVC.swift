d64031e79b2bd732dd6b0da56b5168f6233a1615
Eduardo Herzer <eduardo.herzer@gmail.com>
2020-11-23 21:22:32 -0300

    assets: Add back arrow image

---
+++ Best Sellers/Best Sellers/modules/bestSellerDetails/BestSellerDetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  BestSellerDetailsVC.swift
+//  Best Sellers
+//
+//  Created by Eduardo Herzer on 23/11/20.
+//
+
+import UIKit
+
+class BestSellerDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
